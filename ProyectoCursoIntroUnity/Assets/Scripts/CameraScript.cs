2419ee957f9dc63f2fddbd64227f1a274b799149
AngelGarBar <56541642+AngelGarBar@users.noreply.github.com>
2020-04-15 15:11:51 +0200

    Creacion Proyecto, Terreno, Persona y Script camara

---
+++ ProyectoCursoIntroUnity/Assets/Scripts/CameraScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+
+	public GameObject personaje;
+	public float posX;
+	public float posY;
+	public float posZ;
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		this.gameObject.transform.position = new Vector3(personaje.transform.position.x + posX, personaje.transform.position.y + posY, personaje.transform.position.z+posZ);
+	}
+}
